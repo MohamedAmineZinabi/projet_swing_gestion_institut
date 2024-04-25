9e13f10ba7fbd7137b1c1e9ca09e8ed37d4a60c8
MohamedAmineZinabi <152510981+MohamedAmineZinabi@users.noreply.github.com>
2024-04-25 21:13:38 +0100

    Add files via upload
    hana zdt fiche dial finance ghan9ado db

---
+++ src/gestion_de_cours/Fiche_finance.java
@@ -0,0 +1,42 @@
+package gestion_de_cours;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Fiche_finance extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Fiche_finance frame = new Fiche_finance();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Fiche_finance() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
